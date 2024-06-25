c834a2aba5603d3423c772a9bd83e163a70aca92
Lord <110597375+kd2929@users.noreply.github.com>
2024-06-25 10:10:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("24250238", ""))
-API_HASH = getenv("cb3f118ce5553dc140127647edcf3720", "")
-BOT_TOKEN = getenv("6687465225:AAHF-yb3LNKeFZzJUQEZIj9zgpp6k-o56qo", "")
-OWNER_ID = list(map(int, getenv("6175650047", "").split()))
-MONGO_DB = getenv("mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn", "")
-LOG_GROUP = getenv("-1002065733031", "")
-CHANNEL_ID = int(getenv("-1002065733031", ""))
+API_ID = int(getenv("24250238"))
+API_HASH = getenv("cb3f118ce5553dc140127647edcf3720")
+BOT_TOKEN = getenv("6687465225:AAHF-yb3LNKeFZzJUQEZIj9zgpp6k-o56qo")
+OWNER_ID = list(map(int, getenv("6175650047").split()))
+MONGO_DB = getenv("mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn")
+LOG_GROUP = getenv("-1002065733031")
+CHANNEL_ID = int(getenv("-1002065733031"))
